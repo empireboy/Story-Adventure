744431980e56b1fd78d8d98ab327f55ca38d8c6c
BuildTools <unconfigured@null.spigotmc.org>
2018-02-13 14:44:21 +0100

    Dialogue System V2

--- Assets/Scripts/DialogueSystem/DialogueWriteTest.cs
+++ Assets/Scripts/DialogueSystem/DialogueWriteTest.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class DialogueWriteTest : MonoBehaviour {
 
     private List<string> _dialogueTextArray = new List<string>();
+    private List<string> _dialogueButtonTextArray = new List<string>();
 
     [SerializeField] private DialogueWriter _dialogueWriter;
 
@@ -12,13 +13,16 @@ public class DialogueWriteTest : MonoBehaviour {
         _dialogueTextArray.Add("Hello");
         _dialogueTextArray.Add("How are you?");
         _dialogueTextArray.Add("I am fine");
+
+        _dialogueButtonTextArray.Add("I am fine");
+        _dialogueButtonTextArray.Add("I am fked up");
     }
 
     private void Update()
     {
         if (Input.GetKeyDown("space"))
         {
-            _dialogueWriter.WriterInit(_dialogueTextArray);
+            _dialogueWriter.WriterInit(_dialogueTextArray, _dialogueButtonTextArray);
         }
     }
 }
--- Assets/Scripts/DialogueSystem/DialogueWriter.cs
+++ Assets/Scripts/DialogueSystem/DialogueWriter.cs
@@ -5,12 +5,14 @@ using System.Collections.Generic;
 public class DialogueWriter : MonoBehaviour {
 
     private List<string> _dialogueTextArray = new List<string>();
+    private List<string> _dialogueButtonTextArray = new List<string>();
     private bool _writing = false;
     private bool _endLine = false;
     private int _index = -1;
     private int _charIndex = -1;
     private float _charTimer = 0;
 
+    [SerializeField] private List<GameObject> _buttonArray = new List<GameObject>();
     [SerializeField] private Text _dialogueText;
     [SerializeField] private GameObject _dialogueBox;
     [SerializeField] private float _nextCharTime;
@@ -20,16 +22,30 @@ public class DialogueWriter : MonoBehaviour {
         _nextCharTime = _nextCharTime * Time.deltaTime;
     }
 
-    public void WriterInit(List<string> dialogueTextArray)
+    public void WriterInit(List<string> dialogueTextArray, List<string> dialogueButtonTextArray)
     {
         // Check if dialogue is already writing
         if (!_writing) {
 
-            // Clear the dialogue text array
+            // Clear
             _dialogueTextArray.Clear();
+            _dialogueButtonTextArray.Clear();
 
             // Get dialogue text
             for (int i = 0; i < dialogueTextArray.Count; i++) _dialogueTextArray.Add(dialogueTextArray[i]);
+
+            // Get dialogue button text
+            if (dialogueButtonTextArray != null)
+            {
+                for (int i = 0; i < dialogueButtonTextArray.Count; i++) _dialogueButtonTextArray.Add(dialogueButtonTextArray[i]);
+
+                // Activate buttons
+                for (int i = 0; i < dialogueButtonTextArray.Count; i++)
+                {
+                    _buttonArray[i].GetComponentInChildren<Text>().text = _dialogueButtonTextArray[i];
+                    _buttonArray[i].SetActive(true);
+                }
+            }
 
             // Initialize some variables for writing
             _writing = true;
@@ -105,5 +121,16 @@ public class DialogueWriter : MonoBehaviour {
         _endLine = false;
         _dialogueText.text = "";
         _dialogueBox.SetActive(false);
+        DeactiveButtons();
+    }
+    
+    private void DeactiveButtons()
+    {
+        GameObject[] buttons = GameObject.FindGameObjectsWithTag("Button");
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].SetActive(false);
+        }
     }
 }
